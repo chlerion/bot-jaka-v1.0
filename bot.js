280348be936450088eecb9ee6bff16f87da413fb
chlerion <muhammad.akmal595@gmail.com>
2025-06-29 22:45:19 +0700

    Add /ping route for uptime monitoring

--- bot.js
+++ bot.js
@@ -504,8 +504,13 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Bot JAKA is running!');
+  if (req.url === '/ping') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('pong');
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Bot JAKA is running!');
+  }
 }).listen(port, '0.0.0.0', () => {
   console.log(`Listening on port ${port}`);
 });
